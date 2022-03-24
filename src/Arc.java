9463758a678c868a3b0f602a7bfdd112487f567d
BT_1E10_04 <sample@bths.edu>
2022-03-24 13:55:45 -0400

    Initial commit

---
+++ src/Arc.java
@@ -0,0 +1,14 @@
+public class Arc extends Shape
+{
+    private int startAngle;
+    private int arcAngle;
+
+    public Arc()
+    {
+        super();
+        startAngle = 30;
+        arcAngle = 70;
+    }
+
+    public void drawArc(int x, int y)
+}
